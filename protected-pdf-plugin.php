66cbd8e99ab99e958b39d7bd35df65ab694e13e9
rav1961 <r.rojek87@gmail.com>
2023-11-07 07:14:11 +0100

    first commit

---
+++ protected-pdf-plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @link              https://rcode.pl
+ * @since             1.0.0
+ * @package           Protected_Pdf_Plugin
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Protected PDF File
+ * Plugin URI:        https://pdf.rcode.pl
+ * Description:       Plugin for a secured PDF file in exchange for contact details collected by the form. The selected file remains restricted to protect against unauthorized access.
+ * Version:           1.0.0
+ * Author:            Rafał Rojek
+ * Author URI:        https://rcode.pl
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       protected-pdf
+ * Domain Path:       /languages
+ */
